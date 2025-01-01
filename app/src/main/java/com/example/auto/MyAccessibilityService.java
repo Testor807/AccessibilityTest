508226f1a6f59ee5274707f7e98c036f15b92ba5
Deatherngai <tom2000998@yahoo.com.hk>
2025-01-01 10:25:51 +0800

    first commit

---
+++ app/src/main/java/com/example/auto/MyAccessibilityService.java
@@ -0,0 +1,14 @@
+package com.example.auto;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+public class MyAccessibilityService extends AccessibilityService {
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+    }
+
+    @Override
+    public void onInterrupt() {
+    }
+}
